56a3111037ea8cb12515613e65c8ca88747c06a8
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2020-01-07 18:06:19 -0800

    cors-change

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const server = express()
 
 server.use(express.json())
 
-server.use(cors)
+server.use(cors())
 server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
 server.use('/api/todo', todoRouter)
